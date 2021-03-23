c1000be06020bff49b897f65199f0100ef1db3be
Aaron <ilestkempo@gmail.com>
2021-03-23 09:18:44 -0600

    switch over to Apollo Client on the frontend

--- client/website/pages/index.tsx
+++ client/website/pages/index.tsx
@@ -1,51 +1,51 @@
 import Head from 'next/head'
 import { useState } from 'react'
 import { loadStripe } from '@stripe/stripe-js'
-import { gql } from '@apollo/client'
+import { gql, useMutation } from '@apollo/client'
 import client from '../lib/apolloClient'
 import styles from '../styles/Home.module.css'
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_TEST_STRIPE_KEY);
 
 interface LineItem {
-  id: number;
+  itemId: number;
   quantity: number;
-}
-
-interface MenuItem {
-  id: number;
-  title: string;
-  unitPrice: number;
 }
 
 function formatUnitPrice(unitPrice) {
   return (unitPrice / 100);
 }
 
+const CREATE_CHECKOUT_MUTATION = gql`
+  mutation CreateCheckout($lineItems: [LineItemInput]) {
+    createCheckoutSession(input: {
+      lineItems: $lineItems
+    }) {
+      sessionId
+    }
+  }
+`;
+
 export default function Home(props) {
 
   const [cart, updateCart] = useState<LineItem[]>([]);
+  const [createCheckoutSession] = useMutation(CREATE_CHECKOUT_MUTATION, { 
+    client
+  });
 
   async function handleClick(_) {
-    // TODO: use Apollo Client?
-
     // TODO: refactor out into singleton
     const stripe = await stripePromise;
 
-    // specify the right content-type
-    const response = await fetch("http://localhost:4000/checkout", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(cart)
+    const { data } = await createCheckoutSession({
+      variables: {
+        lineItems: cart
+      }
     });
-
-    const session = await response.json();
 
     // When the customer clicks on the button, redirect them to Checkout.
     const result = await stripe.redirectToCheckout({
-      sessionId: session.id,
+      sessionId: data.createCheckoutSession.sessionId,
     });
 
     if (result.error) {
@@ -60,23 +60,23 @@ export default function Home(props) {
     return (event) => {
       const quantity: number = parseInt(event.target.value);
 
-      const isTracked = cart.some(item => item.id === itemId);
+      const isTracked = cart.some(item => item.itemId === itemId);
 
       if(quantity === 0 && isTracked) {
         // remove item from cart
         const updated = cart.reduce((prev, current) => {
-          return current.id !== itemId ? [...prev, current] : prev;
+          return current.itemId !== itemId ? [...prev, current] : prev;
         }, []);
 
         updateCart(updated);
       }else if (quantity > 0 && !isTracked) {
 
         // append item to the cart
-        updateCart([...cart, { id: itemId, quantity }])
+        updateCart([...cart, { itemId: itemId, quantity }])
       }else if (quantity > 0 && isTracked) {
 
         // update item quantity in the cart
-        updateCart(cart.map(lineItem => lineItem.id === itemId ? { id: itemId, quantity } : lineItem))
+        updateCart(cart.map(lineItem => lineItem.itemId === itemId ? { itemId: itemId, quantity } : lineItem))
       }
     };
   }
