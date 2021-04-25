04bea643e88149775e004b37b0e5e583a1de00cb
Aaron <ilestkempo@gmail.com>
2021-04-24 17:35:56 -0700

    add basic logging andsome action items for the future

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 import Stripe from 'stripe';
 import { Context, getContext } from '../context';
-interface LineItem {
-  itemId: number;
-  quantity: number;
-}
 
 if(!process.env.STRIPE_TEST_KEY || !process.env.WEBHOOK_SECRET) {
   throw new Error('Unable to read environment variables.');
@@ -21,6 +17,22 @@ const routes = express.Router();
 const endpointSecret = process.env.WEBHOOK_SECRET;
 
 const fulfillOrder = async (session: Stripe.Checkout.Session) => {
+  /**
+   * FUTURE UPDATE: 
+   * 
+   * Change from this:
+   * 1. Fetch Stripe LineItems with sessionId
+   * 2. Reduce response to the titles of each LineItem
+   * 3. Fetch the respective Prisma itemId's associated with each title
+   * 4. Use those itemIds to create the proper payload to create an order
+   * 5. Create an order
+   * 
+   * ---
+   * 
+   * To this:
+   * 1. Fetch Stripe LineItems with sessionId
+   * 2. Map to proper payload identifying with the LineItem.id field (match with db itemId)
+   */
 
   console.log('Fulfilling order...');
 
@@ -48,13 +60,9 @@ const fulfillOrder = async (session: Stripe.Checkout.Session) => {
 
   console.log('ids', dbIds);
 
-  // Transform into the right form
-  // We could also the the `connect` relation instead of identifying
-  // by foreign key `itemId` directly. Either works.
-  const toCreate: LineItem[] = lineItems.data.map(stripeItem => {
-
+  const toCreate = lineItems.data.map(stripeItem => {
     return {
-      quantity: stripeItem.quantity ?? 1, // default quantity size, but that should already be listed
+      quantity: stripeItem.quantity ?? 1,
       itemId: dbIds.find(el => el.title === stripeItem.description)!.id
     };
   });
@@ -66,6 +74,9 @@ const fulfillOrder = async (session: Stripe.Checkout.Session) => {
       lineItems: {
         create: toCreate
       }
+    },
+    include: {
+      lineItems: true
     }
   });
 }
@@ -76,6 +87,7 @@ routes.post('/v1/payment/complete', express.raw({ type: 'application/json' }), a
   const sig = req.headers['stripe-signature'];
 
   if(!sig) {
+    console.log('Stripe signature not found');
     return res.status(400).send(`Stripe signature not found.`);
   }
 
@@ -84,6 +96,7 @@ routes.post('/v1/payment/complete', express.raw({ type: 'application/json' }), a
   try {
     event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
   } catch (err) {
+    console.log(err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
   // Handle the checkout.session.completed event
@@ -96,8 +109,7 @@ routes.post('/v1/payment/complete', express.raw({ type: 'application/json' }), a
       // TODO: what if the checkout session is completed, but the order fulfillment fails?
       console.log('Order fulfillment error', err);
     });
-    console.log('Saved below to DB');
-    console.log(res);
+    console.log('Order saved: \n', res);
   }
 
   res.status(200).json({received: true});
@@ -105,8 +117,7 @@ routes.post('/v1/payment/complete', express.raw({ type: 'application/json' }), a
 
 routes.get('/health', (req, res) => {
   res.status(200).json({
-    prisma: ctx.prisma ? true : false,
-    stripe: true
+    prisma: ctx.prisma ? true : false
   })
 })
 
