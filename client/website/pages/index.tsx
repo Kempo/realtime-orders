d93d7eb247296208864ce2bcf83ed4d7b164ed6c
Aaron <ilestkempo@gmail.com>
2021-04-24 17:30:28 -0700

    home page reorganized and a little bit of copy rewritten

--- client/website/pages/index.tsx
+++ client/website/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <div className={styles.logo}>
           <Image src="/main.svg" alt="Cedars of Lebanon Logo" width="490" height="320" />
         </div>
+        <h2>Serving the best gyros, shawarmas, and falafels since 1976.</h2>
         <Link href="/menu">
           <a>
             <button className={styles.orderButton}>
@@ -18,25 +19,26 @@ export default function Home() {
           </a>
         </Link>
       </div>
+      <div id="contact" className={styles.contact}>
+        <h1>
+          Try us out!
+        </h1>
+          <Contact />
+      </div>
+      <div className={styles.picture}>
+        <Image src="https://d7xe6a0v1wpai.cloudfront.net/restaurant.jpeg" alt="Cedars of Lebanon Logo" width="901" height="591" />
+      </div>
       <div id="about" className={styles.about}>
           <h1>About Us</h1>
           <p>
             <b>Cedars of Lebanon</b> has been serving Seattle since 1976. 
             <br /> 
-            We are family-run and serve the best gyros and falafels in town. We use fresh ingredients and take pride in making a delicious meal for you!
+            We make the best gyros and shawarmas in the University District and
+            take pride in providing affordable, delicious, and fresh food to you.
             <br /> 
             <br />
             Come stop by and say hello! We'd love to see you.
           </p>
-      </div>
-      <div className={styles.picture}>
-        <Image src="https://d7xe6a0v1wpai.cloudfront.net/restaurant.jpeg" alt="Cedars of Lebanon Logo" width="901" height="591" />
-      </div>
-      <div id="contact" className={styles.contact}>
-        <h1>
-          Contact Us
-        </h1>
-        <Contact />
       </div>
     </div>
   )
