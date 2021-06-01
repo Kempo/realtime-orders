0631a47d3e6f3001d7ac92e9087e212cc155a3d9
Aaron <ilestkempo@gmail.com>
2021-06-01 08:56:15 -0700

    udpate seeds testing for category and dietary migration

--- server/tests/seeds.test.ts
+++ server/tests/seeds.test.ts
@@ -1,5 +1,7 @@
 import menuItems from '../prisma/seeds/menu';
 
+const CATEGORIES_SIZE = 6, MENU_LENGTH = 29;
+
 describe('proper seeds formatting', () => {
   it('should contain all necessary fields', () => {
     menuItems.forEach((val, i) => {
@@ -7,6 +9,24 @@ describe('proper seeds formatting', () => {
       expect(val.title).not.toBeUndefined();
       expect(val.unitPrice).not.toBeUndefined();
       expect(val.unitPrice).toBeGreaterThan(0);
+      expect(val.category).not.toBeUndefined();
     });
+
   });
+
+  it('should have the correct fields and categories', () => {
+    const categories: Set<String> = new Set();
+    menuItems.forEach(curr => {
+      if(!curr.category || !curr.dietary) {
+        fail('Menu item should have category and dietary field.');
+      }
+      categories.add(curr.category);
+    });
+
+    expect(categories.size).toBe(CATEGORIES_SIZE);
+  });
+
+  it('should have the correct menu length', () => {
+    expect(menuItems.length).toBe(MENU_LENGTH);
+  })
 });
